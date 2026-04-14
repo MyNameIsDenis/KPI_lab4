88cdab168270b7f0e67dfa108ba9702147ef99b1
Denis . <dendj07@gmail.com>
2026-04-14 16:25:51 +0300

    implement priority comparison in findIndex

--- Lab4.js
+++ Lab4.js
@@ -9,6 +9,24 @@ class PriorityQueue {
   findIndex(identifier) {
     if (identifier === "oldest") return 0;
     if (identifier === "newest") return this.queue.length - 1;
+
+    let index = 0;
+    let compare;
+
+    if (identifier === "highest") {
+      compare = (a, b) => a > b;
+    } else if (identifier === "lowest") {
+      compare = (a, b) => a < b;
+    } else {
+      throw new Error("Crirteium doesn't exist");
+    }
+
+    for (let i = 1; i < this.queue.length; i++) {
+      if (compare(this.queue[i].priority, this.queue[index].priority)) {
+        index = i;
+      }
+    }
+    return index;
   }
 }
 
