94567b690f91c3ff25f2a7208b3ccfd2ceec09cb
Denis . <dendj07@gmail.com>
2026-04-14 16:16:18 +0300

    add findIndex base structure

--- Lab4.js
+++ Lab4.js
@@ -2,9 +2,14 @@ class PriorityQueue {
   queue = [];
 
   enqueue(item, priority) {
-  this.queue.push({ value: item, priority: priority });
-}
+    this.queue.push({ value: item, priority: priority });
+  }
 
+
+  findIndex(identifier) {
+    if (identifier === "oldest") return 0;
+    if (identifier === "newest") return this.queue.length - 1;
+  }
 }
 
 export default PriorityQueue;
